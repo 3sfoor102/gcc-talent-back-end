0b21c89ab5e70dc9a61268cf35f1a84df234b828
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-26 21:40:40 +0300

    Add generate Access Token function with linke the env file

--- server/src/modules/auth/auth.service.js
+++ server/src/modules/auth/auth.service.js
@@ -5,3 +5,7 @@ const bcrypt = require('bcrypt')
 const User = require('../../models/User')
 
 
+const generateAccessToken = function (id, role)
+{
+    return jwt.sign({ id, role }, process.env.JWT_ACCESS_SECRET, { expiresIn: process.env.JWT_ACCESS_EXPIRES })
+}
