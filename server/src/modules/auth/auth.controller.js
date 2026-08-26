0cb0c1c5dd63cf9ad857cf02e38a9cf1cd46b60b
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-26 22:46:17 +0300

    Add the register function with the all requirment as the DB and add the try catch with the specific error number

--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -1,1 +1,41 @@
 const authService = require('./auth.service')
+
+const register = async function (req, res)
+{
+    try {
+
+        const { name, email, password, role } = req.body
+
+        const { user, accessToken } = await authService.registerUser({ name, email, password, role })
+
+        res.status(201).json({
+            success: true,
+            data: {
+                user: {
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    role: user.role
+                },
+                accessToken
+            }
+        })
+    } catch (err)
+    {
+        res.status(400).json({
+            success: false,
+            error: { message: err.message }
+        })
+    }
+}
+
+
+
+
+
+
+
+
+module.exports = {
+    register,
+}
