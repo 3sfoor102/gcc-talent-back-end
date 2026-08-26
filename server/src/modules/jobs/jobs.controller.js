e554afe0fe6440637b55710e0f0f37503173cb5a
Hasan Ali <hassanalsurh@gmail.com>
2026-08-26 20:10:46 +0300

    Creating the indexJob function to retrieve all available jobs

--- server/src/modules/jobs/jobs.controller.js
+++ server/src/modules/jobs/jobs.controller.js
@@ -2,7 +2,17 @@ const Job = require('../../models/Job')
 
 
 
-const indexJob = async (req, res) => { }
+const indexJob = async (req, res) => {
+    try {
+        const jobs = await Job.find()
+
+        res.status(200).json(jobs)
+
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+
+    }
+}
 const createJob = async (req, res) => { }
 const updateJob = async (req, res) => { }
 const deleteJob = async (req, res) => { }
