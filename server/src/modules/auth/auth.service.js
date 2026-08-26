b6615a76f6c6c787449a414d3bcfaa4d1aa55f92
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-26 22:29:33 +0300

    Add the login User function and export the function

--- server/src/modules/auth/auth.service.js
+++ server/src/modules/auth/auth.service.js
@@ -23,7 +23,7 @@ const registerUser = async function (userData)
     }
 
     const salt = await bcrypt.genSalt(10)
-    
+
     const passwordHash = await bcrypt.hash(password, salt)
 
     const user = await User.create({ name, email, passwordHash, role })
@@ -34,7 +34,29 @@ const registerUser = async function (userData)
 }
 
 
+const loginUser = async function (email, password)
+{
+    const user = await User.findOne({ email }).select('+passwordHash')
+
+    if (!user)
+    {
+        throw new Error('Invalid email or password')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.passwordHash)
+
+    if (!isMatch)
+    {
+        throw new Error('Invalid email or password')
+    }
+
+    const accessToken = generateAccessToken(user._id, user.role)
+
+    return { user, accessToken }
+}
+
 
 module.exports = {
     registerUser,
+    loginUser
 }
