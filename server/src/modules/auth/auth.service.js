e0d4ebaf5b33ea414f0468ce8dfdfe79a1bae850
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-26 22:18:03 +0300

    Add the register function  and export the function

--- server/src/modules/auth/auth.service.js
+++ server/src/modules/auth/auth.service.js
@@ -9,3 +9,32 @@ const generateAccessToken = function (id, role)
 {
     return jwt.sign({ id, role }, process.env.JWT_ACCESS_SECRET, { expiresIn: process.env.JWT_ACCESS_EXPIRES })
 }
+
+
+const registerUser = async function (userData)
+{
+    const { name, email, password, role } = userData
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists)
+    {
+        throw new Error('Email is already registered')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const user = await User.create({ name, email, passwordHash, role })
+
+    const accessToken = generateAccessToken(user._id, user.role)
+
+    return { user, accessToken }
+}
+
+
+
+module.exports = {
+    registerUser,
+}
