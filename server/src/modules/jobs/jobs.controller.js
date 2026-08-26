ff96263b9e74021c90bf0a448297c58aa53e53b5
Hasan Ali <hassanalsurh@gmail.com>
2026-08-26 16:47:29 +0300

    Adding cloudinary and multer config files and structure for the Job feature

---
+++ server/src/modules/jobs/jobs.controller.js
@@ -0,0 +1,16 @@
+const Job = require('../../models/Job')
+
+
+
+const indexJob = async (req, res) => { }
+const createJob = async (req, res) => { }
+const updateJob = async (req, res) => { }
+const deleteJob = async (req, res) => { }
+
+
+module.exports = {
+    indexJob,
+    createJob,
+    updateJob,
+    deleteJob
+}
