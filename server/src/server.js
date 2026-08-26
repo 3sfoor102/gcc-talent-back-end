8929cffda17a981e4c7e4adbe859060701ac9f9e
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-26 21:18:34 +0300

    Add the verify-token middileware into server

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const dns = require("node:dns")
 dns.setServers(["8.8.8.8", "1.1.1.1"])
 
-
+const verifyToken = require('./middleware/verify-token')
 
 require('dotenv').config()
 const express = require('express')
