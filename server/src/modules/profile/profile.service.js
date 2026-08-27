bf26795b278af208eb14d8444f42ca5eea9f6fe1
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-27 10:54:12 +0300

    Add the create freelancer profile function into profile service and export it

--- server/src/modules/profile/profile.service.js
+++ server/src/modules/profile/profile.service.js
@@ -3,4 +3,22 @@ const FreelancerProfile = require('../../models/FreelancerProfile')
 const ClientProfile = require('../../models/ClientProfile')
 
 
+const createFreelancerProfile = async function (userId, profileData) 
+{
 
+    const existingProfile = await FreelancerProfile.findOne({ user: userId })
+
+    if (existingProfile) 
+    {
+        throw new Error('Freelancer profile already exists')
+    }
+
+    const profile = await FreelancerProfile.create({ user: userId, ...profileData })
+
+    return profile
+}
+
+
+module.exports = {
+    createFreelancerProfile
+}
