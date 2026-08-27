0d11711d06eee061c7fbbdebb4289b4a03f5e00c
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-27 09:55:03 +0300

    Change the name of JWT_ACCESS_SECRET to look like env and fix it

--- server/src/middleware/verify-token.js
+++ server/src/middleware/verify-token.js
@@ -3,9 +3,11 @@ const jwt = require('jsonwebtoken')
 const verifyToken = function (req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1]
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+        const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
 
-        req.user = decoded.payload
+        req.user = decoded
+        
         next()
     } catch (err) {
         res.status(401).json({ err: 'Invalid token' })
