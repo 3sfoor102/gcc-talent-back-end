a5b4611bd3e2fabf421530b138c2860a3301f411
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-29 00:38:16 +0300

    Add get User Notifications function into service

--- server/src/modules/notification/notification.service.js
+++ server/src/modules/notification/notification.service.js
@@ -1,2 +1,13 @@
 const Notification = require('../../models/Notification')
 
+const getUserNotifications = async function (userId)
+{
+    const notifications = await Notification.find({ user: userId }).sort({ createdAt: -1 })
+    
+    return notifications
+}
+
+
+module.exports = {
+    getUserNotifications,
+}
