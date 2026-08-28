3c10e8f2af1ebf6075423d952128cb2844ba32de
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-29 01:05:33 +0300

    Add get Notifications ctrl with the error handler

--- server/src/modules/notification/notification.controller.js
+++ server/src/modules/notification/notification.controller.js
@@ -1,1 +1,27 @@
 const notificationService = require('./notification.service')
+
+const getNotifications = async function (req, res, next)
+{
+    try {
+        const userId = req.user.id
+
+        const notifications = await notificationService.getUserNotifications(userId)
+
+        res.status(200).json({
+            success: true,
+            data: {
+                notifications
+            }
+        })
+    }
+    catch (err)
+    {
+        res.status(400)
+        next(err)
+    }
+}
+
+
+module.exports = {
+    getNotifications,
+}
