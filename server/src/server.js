b4713232c6964dd8fe5ccc4c349a5ce5983b1914
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-28 20:27:33 +0300

    Add the errorhandler middleware into server

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const dns = require("node:dns")
 dns.setServers(["8.8.8.8", "1.1.1.1"])
 
+const errorHandler = require('./middleware/errorHandler')
 const verifyToken = require('./middleware/verify-token')
 const authCtrl = require('./modules/auth/auth.controller')
 const profileCtrl = require('./modules/profile/profile.controller')
@@ -39,6 +40,7 @@ app.put('/profile/freelancer', verifyToken, profileCtrl.updateFreelancer)
 app.get('/profile/client', verifyToken, profileCtrl.getClient)
 app.put('/profile/client', verifyToken, profileCtrl.updateClient)
 
+app.use(errorHandler)
 
 app.listen(PORT, function () {
     console.log(`The express app is ready on port ${PORT}! ✨✨✨`)
