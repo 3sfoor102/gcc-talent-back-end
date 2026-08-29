c03fcf63c3bb81644690777f41c875e1114232be
3sfoor102 <3sfoor102@gmail.com>
2026-08-29 18:46:33 +0300

    getContracts is ready

--- server/src/modules/contracts/contracts.controller.js
+++ server/src/modules/contracts/contracts.controller.js
@@ -4,7 +4,17 @@ const getContracts = async (req, res, next) => {
     try {
         const userId = req.user._id
         const role = req.user.role
+        const {status} = req.query
+
+        const result = await contractService.getUserContracts(userId, role, status)
+
+        res.status(200).json({
+            success: true, 
+            data: result,
+     })
+
     } catch (error) {
-        
+        next(error)
     }
+    
 }
