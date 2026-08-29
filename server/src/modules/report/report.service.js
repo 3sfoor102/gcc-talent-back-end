d662410e6d3d24211078d5487b10bb5e26d4d11f
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-29 16:08:45 +0300

    Add create Report into services and export it

--- server/src/modules/report/report.service.js
+++ server/src/modules/report/report.service.js
@@ -1,2 +1,23 @@
 const Report = require('../../models/Report')
 
+const createReport = async function (reporterId, reportData)
+{
+    const report = new Report(
+    {
+        reporter: reporterId,
+
+        targetType: reportData.targetType,
+
+        targetId: reportData.targetId,
+
+        reason: reportData.reason
+    })
+
+    await report.save()
+    
+    return report
+}
+
+module.exports = {
+    createReport,
+}
