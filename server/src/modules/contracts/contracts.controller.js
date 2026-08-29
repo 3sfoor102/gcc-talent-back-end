3073e7a7e7e6bc35eec558908415bfc6d4e5b4b7
3sfoor102 <3sfoor102@gmail.com>
2026-08-29 18:59:36 +0300

    finished main functions in contracts

--- server/src/modules/contracts/contracts.controller.js
+++ server/src/modules/contracts/contracts.controller.js
@@ -24,6 +24,9 @@ const getContractsById = async (req, res, next)=> {
         const contractId = req.params._id
         const userId = req.user._idc
         const role = req.user.role
+
+        const result = await contractService.getContractWorkspace(contractId, userId, role)
+
         res.status(200).json({
         success: true, 
         data: result,
@@ -31,5 +34,53 @@ const getContractsById = async (req, res, next)=> {
     } catch (error) {
         next(error)
     }
+}
+const addMilestone = async (req, res, next) => {
+  try {
+    const contractId = req.params.id;
+    const clientId = req.user._id;
+    const milestoneData = req.body; 
+
+    const result = await contractsService.addMilestone(contractId, clientId, milestoneData);
+
+    res.status(201).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+const fundMilestone = async (req, res, next) => {
+  try {
+    const {id: contractId, mid: milestoneId} = req.params
+    const clientId = req.user._id
+
+    const result = await contractsService.fundMilestone(contractId, milestoneId, clientid);
+
+    res.status(201).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+const deliverMilestone = async (req, res, next) => {
+  try {
+    const {id: contractId, mid: milestoneId} = req.params
+    const freelancerId = req.user._id
+    const deliveryData = req.body
+
+    const result = await contractsService.deliverMilestone(contractId, milestoneId, freelancerId, deliveryData);
+
+    res.status(201).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
