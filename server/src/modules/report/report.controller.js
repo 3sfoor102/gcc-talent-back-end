003ef97b18fe5741ef745868e0becaaa923933e9
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-29 16:31:52 +0300

    Add the submit Report function into ctrl with error handler

--- server/src/modules/report/report.controller.js
+++ server/src/modules/report/report.controller.js
@@ -1,2 +1,30 @@
 const reportService = require('./report.service')
 
+const submitReport = async function (req, res, next)
+{
+    try {
+
+        const userId = req.user.id
+
+        const reportData = req.body
+
+        const report = await reportService.createReport(userId, reportData)
+
+        res.status(201).json({
+            success: true,
+            data: {
+                report
+            }
+        })
+    }
+    catch (err)
+    {
+        res.status(400)
+        next(err)
+    }
+}
+
+
+module.exports = {
+    submitReport,
+}
