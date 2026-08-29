efa01c425cc7dbc68038150e7738a56d059efa41
3sfoor102 <3sfoor102@gmail.com>
2026-08-29 16:44:48 +0300

    working on processDeposit

--- server/src/modules/wallet/wallet.service.js
+++ server/src/modules/wallet/wallet.service.js
@@ -4,9 +4,20 @@ const Transaction = require('../../models/Transaction.js')
 
 const getUserWallet = async (userId) => {
     const user = await findById(userId).select('wallet')
+    if (!user) {
+        const error = new Error('User not found!')
+        error.statusCode = 404
+        throw error
+    }
+
+    const transactions = await Transaction.find({user: userId}).sort({createdAt: -1})
+    return {
+        wallet: user.wallet, 
+        transactions: transactions
+}
 }
 
-if (!user) {
-    const error = new Error('User not found!')
-
+const processDeposit = async(userId, amount, cardNumber) => {
+    if
 }
+
