6d83a1737b33122dcb8ee9a9afd661962ed5cfe0
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-29 17:25:21 +0300

    Add the get Settings function into ctrl with the error handler and export it

--- server/src/modules/setting/setting.controller.js
+++ server/src/modules/setting/setting.controller.js
@@ -1,1 +1,26 @@
 const settingService = require('./setting.service')
+
+
+const getSettings = async function (req, res, next)
+{
+    try {
+        const settings = await settingService.getAllSettings()
+
+        res.status(200).json({
+            success: true,
+            data: {
+                settings
+            }
+        })
+    }
+    catch (err)
+    {
+        res.status(400)
+        next(err)
+    }
+}
+
+
+module.exports = {
+    getSettings,
+}
