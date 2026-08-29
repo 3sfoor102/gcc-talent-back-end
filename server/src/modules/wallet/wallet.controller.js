d1e256afa98423b7f38d35a16533721eee117888
3sfoor102 <3sfoor102@gmail.com>
2026-08-29 15:49:51 +0300

    complete getWallet

--- server/src/modules/wallet/wallet.controller.js
+++ server/src/modules/wallet/wallet.controller.js
@@ -1,23 +1,46 @@
-const express = require("express")
 
-const walletService = require('../services/wallet.service.js');
-
-const deposit = async (req, res, next) => {
+const walletService = require("../services/wallet.service.js");
+const wallet
+const getWallet = async (req, res, next) => {
     try {
-        const userId = req.user._id; 
-        
-        const { amount, card } = req.body;
-
-        const result = await walletService.processDeposit(userId, amount, card);
+        const userId = req.user._id
+        const result = await walletService.getUserWallet(userId)
 
         res.status(200).json({
             success: true,
-            data: result
-        });
-        
+            data: result,
+        })
     } catch (error) {
-        next(error);
+        next(error)
     }
+}
+
+
+const deposit = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    const { amount, card } = req.body;
+
+    const result = await walletService.processDeposit(userId, amount, card);
+
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports = { deposit };
+const withdraw = async (req, res, next) => {
+    try {
+        const userId = req.user._id
+        
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = { depositو getWallet, withdraw };
