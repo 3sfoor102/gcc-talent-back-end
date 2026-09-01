e365595c70b2536595adda1b1655887598bffd03
Hasan Ali <hassanalsurh@gmail.com>
2026-09-01 16:44:14 +0300

    Updating some routes

--- server/src/modules/proposals/proposals.routes.js
+++ server/src/modules/proposals/proposals.routes.js
@@ -6,16 +6,17 @@ const verifyToken = require('../../middleware/verify-token')
 
 const proposalCTRL = require('./proposals.controller')
 
-// Freelancer routes
-router.get('/mine', verifyToken, authorize('freelancer'), proposalCTRL.getMyProposals)
-router.post('/jobs/:jobId', verifyToken, authorize('freelancer'), proposalCTRL.createProposal)
-router.put('/:proposalId', verifyToken, authorize('freelancer'), proposalCTRL.updateProposal)
-router.patch('/:proposalId/withdraw', verifyToken, authorize('freelancer'), proposalCTRL.withdrawProposal)
 
-// Client routes
-router.get('/jobs/:jobId', verifyToken, authorize('client'), proposalCTRL.getJobProposals)
-router.patch('/:proposalId/accept', verifyToken, authorize('client'), proposalCTRL.acceptProposal)
-router.patch('/:proposalId/shortlist', verifyToken, authorize('client'), proposalCTRL.shortlistProposal)
-router.patch('/:proposalId/decline', verifyToken, authorize('client'), proposalCTRL.declineProposal)
+router.use(verifyToken)
+
+// Freelancer routes
+router.get('/mine', authorize('freelancer'), proposalCTRL.getMyProposals)
+router.patch('/:proposalId', authorize('freelancer'), proposalCTRL.updateProposal)
+router.post('/:proposalId/withdraw', authorize('freelancer'), proposalCTRL.withdrawProposal)
+
+// Client decision routes (POST per Section 09)
+router.post('/:proposalId/accept', authorize('client'), proposalCTRL.acceptProposal)
+router.post('/:proposalId/shortlist', authorize('client'), proposalCTRL.shortlistProposal)
+router.post('/:proposalId/decline', authorize('client'), proposalCTRL.declineProposal)
 
 module.exports = router
