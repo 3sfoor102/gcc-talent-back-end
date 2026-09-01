9fc01bf92bc052c463ecdb688434939322470d80
AliAbdulla2002 <alisaleh.contact@gmail.com>
2026-09-01 19:22:38 +0300

    Add the update Avatar function

--- server/src/modules/setting/setting.controller.js
+++ server/src/modules/setting/setting.controller.js
@@ -1,4 +1,6 @@
 const settingService = require('./setting.service')
+const User = require('../../models/User')
+const cloudinary = require('../../config/cloudinary')
 
 
 const getSettings = async function (req, res, next)
@@ -45,7 +47,64 @@ const updateSetting = async function (req, res, next)
 }
 
 
+const updateAvatar = async function (req, res, next) 
+{
+    try {
+
+        if (!req.file) 
+        {
+            return res.status(400).json({ success: false, message: 'Please upload an image' })
+        }
+
+        const userId = req.user.id
+
+        const uploadStream = cloudinary.uploader.upload_stream(
+            { 
+                folder: "gcc_talent/avatars", 
+                transformation: [{ width: 250, height: 250, crop: "fill" }]
+            },
+            async function (error, result){
+                if (error) {
+                    console.error("Cloudinary Upload Error:", error)
+                    return res.status(500).json({ success: false, message: 'Failed to upload image' })
+                }
+
+                const user = await User.findById(userId)
+                if (!user) {
+                    return res.status(404).json({ success: false, message: 'User not found' })
+                }
+
+                user.avatarUrl = result.secure_url
+                await user.save()
+
+                return res.status(200).json({
+                    success: true,
+                    message: 'Avatar updated successfully',
+                    data: {
+                        user: {
+                            _id: user._id,
+                            name: user.name,
+                            email: user.email,
+                            role: user.role,
+                            avatarUrl: user.avatarUrl
+                        }
+                    }
+                })
+            }
+        )
+
+        uploadStream.end(req.file.buffer)
+
+    } catch (err) 
+    {
+        res.status(400)
+        next(err)
+    }
+}
+
+
 module.exports = {
     getSettings,
-    updateSetting
+    updateSetting,
+    updateAvatar
 }
