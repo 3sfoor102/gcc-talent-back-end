c47808883cd491c95bb0cadaf5e03a22bbfdfbd5
AliAbdulla2002 <alisaleh.contact@gmail.com>
2026-09-04 14:40:43 +0300

    Fix

--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -150,7 +150,7 @@ const socialLogin = async function (req, res, next) {
 const linkedinLogin = async function (req, res, next) {
     try {
         const { code } = req.body;
-        const redirectUri = 'http://localhost:5173/linkedin';
+        const redirectUri = `${req.headers.origin || 'https://ga-gcc-talent.vercel.app'}/linkedin`;
 
         const tokenResponse = await fetch('https://www.linkedin.com/oauth/v2/accessToken', {
             method: 'POST',
